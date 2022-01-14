83b91444a30fe0a8853a0712c0797c5181dd774c
yarichao <yari.onesoul@gmail.com>
2022-01-14 17:56:19 +0800

    define spec

--- __test__/todo.test.js
+++ __test__/todo.test.js
@@ -7,3 +7,12 @@ it('should render TodoPage', function () {
     render(<TodoPage />)
     expect(screen.getByText(/TodoList/i)).toBeInTheDocument()
 });
+
+it('should add a dummy todo', function () {
+    render(<MockTodoPage/>)
+
+    // addDummyTodo
+
+    // expect Dummy Todo in the document
+    expect(screen.getByText(/Dummy Todo/i)).toBeInTheDocument()
+});
